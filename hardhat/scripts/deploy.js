31e83362c7fff89cc2ba07c2d98b35597dc25ffd
thecodingliva <justnur13@gmail.com>
2023-01-20 16:12:00 +0300

    first commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  const metadataURL = "https://ipfs.io/ipfs/Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
+
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
